d8753ed3a9b0b62338bd817709749982549b8359
tenoriocesar <tenoriocesar@gmail.com>
2021-09-14 18:29:47 +0000

    vnet fix

--- vnet.tf
+++ vnet.tf
@@ -1,13 +1,13 @@
-# module "vnet" {
-#   source  = "app.terraform.io/josecosta-training/vnet/azurerm"
-#   version = "2.5.0"
-#   # insert required variables here
-#   resource_group_name = azurerm_resource_group.myresourcegroup.name
-# }
-
-module "network" {
-  source  = "Azure/network/azurerm"
-  version = "3.5.0"
-  # insert the 1 required variable here
+module "vnet" {
+  source  = "app.terraform.io/josecosta-training/vnet/azurerm"
+  version = "2.5.0"
+  # insert required variables here
   resource_group_name = azurerm_resource_group.myresourcegroup.name
 }
+
+# module "network" {
+#   source  = "Azure/network/azurerm"
+#   version = "3.5.0"
+#   # insert the 1 required variable here
+#   resource_group_name = azurerm_resource_group.myresourcegroup.name
+# }
