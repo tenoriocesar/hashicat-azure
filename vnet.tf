758f4a8e08d171618340a880efee277d84a51b1a
tenoriocesar <tenoriocesar@gmail.com>
2021-09-14 18:10:52 +0000

    Added vnet module

--- vnet.tf
+++ vnet.tf
@@ -2,4 +2,5 @@ module "vnet" {
   source  = "app.terraform.io/josecosta-training/vnet/azurerm"
   version = "2.5.0"
   # insert required variables here
+  resource_group_name = azurerm_resource_group.myresourcegroup.name
 }
